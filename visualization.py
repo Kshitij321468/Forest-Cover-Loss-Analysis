62b5e45f72f6f7fb2a6934e8d454710cc106b466
HustleMannia <jmd8199@gmail.com>
2024-12-03 16:20:29 +0530

    Update visualization.py
    Signed-off-by: HustleMannia <jmd8199@gmail.com>

--- visualization.py
+++ visualization.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load the cleaned dataset
-file_path = "../data/crime_data_cleaned.csv"
+file_path = "../data/Forest_cover_cleaned.csv"
 data = pd.read_csv(file_path)
 
 # Display the first few rows of the dataset
@@ -15,12 +15,11 @@ plt.figure(figsize=(10, 6))
 sns.lineplot(data=data, x='year', y='total_cases', hue='region')
 plt.title("Crime Trends by Region")
 plt.xlabel("Year")
-plt.ylabel("Total Cases")
 plt.legend(title="Region")
-plt.savefig("../visuals/crime_trends_by_region.png")
+plt.savefig("../visuals/Forest_cover_loss_region.png")
 plt.show()
 
-# Visualization 2: Bar plot of total crimes by region
+# Visualization 2: Bar plot a forest cover loss  by region
 region_crimes = data.groupby('region')['total_cases'].sum().reset_index()
 plt.figure(figsize=(8, 6))
 sns.barplot(data=region_crimes, x='region', y='total_cases')
@@ -28,5 +27,5 @@ plt.title("Total Crimes by Region")
 plt.xlabel("Region")
 plt.ylabel("Total Cases")
 plt.xticks(rotation=45)
-plt.savefig("../visuals/crime_total_by_region.png")
+plt.savefig("../visuals/Forest_cover_Region/region.png")
 plt.show()
